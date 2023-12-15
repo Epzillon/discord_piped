536a479cd6c8fac28af61e34dd7e70ea0a70770a
Jakob Eriksson <jakoberiksson99@hotmail.com>
2023-12-15 01:11:42 +0100

    feat: add initial structure design and startup for bot

---
+++ index.js
@@ -0,0 +1,50 @@
+// Register Node core functionality
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./src/config/config.json');
+
+// Register application functionality
+const srcPath = path.join(__dirname, '/src');
+const commandsPath = srcPath + "/commands";
+const eventsPath = srcPath + "/events";
+
+// Register discord bot functionality
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// Read and register all commands inside src/commands/
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+// Read and register all events inside src/events/
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    // Register event if it contains a correctly execute method 
+    if ('execute' in event) {
+        console.log("Registering " + event.name + "...")
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    } else {
+        console.log(`[WARNING] The event at ${filePath} is missing a required "execute" property.`);
+    }
+}
+
+// Log in to Discord with your client's token
+client.login(token);
